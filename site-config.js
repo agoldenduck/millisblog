41ceaae12666cd87461515cc8db15b3d45bda752
Alex Dacre <alex@alexdacre.com>
2018-09-10 15:10:35 +0200

    initial commit

---
+++ site-config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  siteTitle: `Amelia lives Danishly Blog`,
+  siteTitleShort: `Milli's Blog`,
+  siteDescription: `Thoughts and reflections while living in Denmark.`,
+  siteUrl: `https://gu.fabianschultz.com`,
+  themeColor: `#000`,
+  backgroundColor: `#fff`,
+  pathPrefix: null,
+  logo: path.resolve(__dirname, 'src/images/icon.png'),
+  social: {
+    twitter: `gatsbyjs`,
+    fbAppId: `966242223397117`,
+  },
+};
